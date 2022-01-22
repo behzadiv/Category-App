9b8504e6e766f328a75dce385fdb25a7325f88bf
Behzad <saeedibehzad@gmail.com>
2022-01-22 13:46:42 +0330

    add productList heading

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,18 +1,20 @@
 import { useProducts, useProductsActions } from "./CategoryProvider";
 import "../App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSortAlphaDown, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { faSortAlphaDown, faTrashAlt,faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 
 const Product = ({ product, onDelete ,onIncrement}) => {
   const trash = <FontAwesomeIcon icon={faTrashAlt} />;
+  const plus = <FontAwesomeIcon icon={faPlus} />;
+  const minus = <FontAwesomeIcon icon={faMinus} />;
   return (
     <div className="product-item">
       <span>{product.name}</span>
       <span>{product.category}</span>
       <span className="edit-btn">
-        <span onClick={onIncrement} className="increment">+</span>
+        <span onClick={onIncrement} className="increment">{plus}</span>
         <span>{product.qty}</span>
-        <span onClick={onDelete} className="decrement">{product.qty> 1 ? "-" : trash}</span>
+        <span onClick={onDelete} className="decrement">{product.qty> 1 ? minus : trash}</span>
       </span>
     </div>
   );
